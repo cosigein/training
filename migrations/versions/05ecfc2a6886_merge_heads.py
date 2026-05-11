fddf05caeb872fbddfdfe18b7341beda634bd54a
Joel H. <143587522+Joelh123@users.noreply.github.com>
2026-05-11 09:03:50 +0200

    feat(profile): add back button icon to edit profile view

---
+++ migrations/versions/05ecfc2a6886_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: 05ecfc2a6886
+Revises: 240717617f45, b9a236562c45
+Create Date: 2026-05-08 13:12:58.322492
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '05ecfc2a6886'
+down_revision = ('240717617f45', 'b9a236562c45')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
